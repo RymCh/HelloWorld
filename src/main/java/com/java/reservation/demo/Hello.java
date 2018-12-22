8e2891ac4154e5b3e88ba12dde6c6bba74dbe592
RymCh <chattiriim@gmail.com>
2018-12-22 13:51:06 +0100

    Update Hello.java

--- src/main/java/com/java/reservation/demo/Hello.java
+++ src/main/java/com/java/reservation/demo/Hello.java
@@ -10,5 +10,9 @@ public class Hello {
     public String sayHello(@RequestParam(value="firstName") String firstName){
     return "Hello "+firstName;
 }
-
+// so your url will look like this: ...../hello?firstName=Rim
+// but if you want it to look like this instead : /hello/Rim then you ought to do like this:
+    // @RequestMapping("/hello/{fistName}")
+    //public String sayHello(@PathVariable("firstName") String firstName) {..}
+    
 }
